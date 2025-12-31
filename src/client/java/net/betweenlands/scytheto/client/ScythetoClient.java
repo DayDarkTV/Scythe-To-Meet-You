c591ddebe09bdd47f95888cc681902e7ed21e862
DayDarkTV <67245492+DayDarkTV@users.noreply.github.com>
2025-12-31 13:56:26 -0500

    init

---
+++ src/client/java/net/betweenlands/scytheto/client/ScythetoClient.java
@@ -0,0 +1,10 @@
+package net.betweenlands.scytheto.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScythetoClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/betweenlands/scytheto/Scytheto.java
@@ -0,0 +1,10 @@
+package net.betweenlands.scytheto;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Scytheto implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
